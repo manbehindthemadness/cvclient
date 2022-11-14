67611fd380604a01f756bc2ebc05c27b98b9d834
Kevin Eales <kev0.0.0.255@gmail.com>
2022-11-14 10:22:22 -0500

    Updated Docstrings.

--- cvc.py
+++ cvc.py
@@ -2,9 +2,6 @@
 """
 This file is part of the cvclient package.
 Copyright (c) 2021 Kevin Eales.
-
-This program is experimental and proprietary, redistribution is prohibited.
-Please see the license file for more details.
 ------------------------------------------------------------------------------------------------------------------------
 
 This is the public facing API client.
